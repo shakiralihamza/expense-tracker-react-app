ed295e37b2797617a4a35599da9de62b6ef5d7ac
shakiralihamza <33418660+shakiralihamza@users.noreply.github.com>
2020-06-12 16:52:27 +0500

    prepared user interface

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Balance from './Balance';
+import IncomeExpenses from './incomeExpenses';
+import TransactionList from './TransactionList';
+import AddTransaction from './AddTransaction';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <div className="container">
+          <Balance />
+          <IncomeExpenses />
+          <TransactionList/>
+          <AddTransaction/>
+      </div>
     </div>
   );
 }
---
+++ src/Balance.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Balance() {
+  return (
+    <>
+        <h4>Your Balance</h4>
+        <h1>$0.00</h1>
+    </>
+  );
+}
+ export default Balance;
